0c9c43fa836abf0a3e422547bfbc679b1433ffb9
kagiso-analyst <matloukagiso0506@gmail.com>
2026-06-28 01:03:44 +0200

    fix errors

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
+import path from "node:path";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  turbopack: {
+    root: path.resolve(__dirname),
+  },
+  allowedDevOrigins: ["172.20.10.4"],
 };
 
 export default nextConfig;
