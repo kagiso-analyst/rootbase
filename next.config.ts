e8973c78f72c19a4b4b12d586dfd9d2fae8258b2
kagiso-analyst <matloukagiso0506@gmail.com>
2026-07-10 13:51:59 +0200

    Update next.config

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: false,
+  },
+  turbopack: {
+    root: path.resolve(__dirname),
   },
 };
 
